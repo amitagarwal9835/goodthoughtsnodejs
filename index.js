33af812ce3135947efb1ade258ed8163f6603f27
amitagarwal9835 <78593171+amitagarwal9835@users.noreply.github.com>
2021-10-12 12:34:59 +0530

    ee2222

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/postApi', (req, res) => {
-    console.log('Hello World from node server post api v1!')
+    console.log('Hello World from node server post api v11!')
     res.send('Hello World from node server post api!')
 })
 
