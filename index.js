0a24c9b635a75e7cfdcbbfbb877e41de4f1441d7
amitagarwal9835 <78593171+amitagarwal9835@users.noreply.github.com>
2021-10-12 11:54:27 +0530

    Add post Api

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello World from node server!')
 })
 
+app.post('/postApi', (req, res) => {
+    console.log('Hello World from node server post api!')
+    res.send('Hello World from node server post api!')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
